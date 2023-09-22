ee36be135cef31dcdb52fdcbfb2e8cd9a17591d8
Charles Bugayer <chaimbugayer2024@u.northwestern.edu>
2023-09-21 22:31:41 -0500

    show a list of courses

---
+++ src/components/Banner.jsx
@@ -0,0 +1,4 @@
+const Banner = ({title}) => (
+    <h1>{title}</h1>
+  );
+export default Banner;
---
+++ src/components/CourseList.jsx
@@ -0,0 +1,6 @@
+const CourseList = ({course}) => (
+    <div>
+        {`${course.term} CS ${course.number}: ${course.title}`}
+    </div>
+    );
+export default CourseList;
