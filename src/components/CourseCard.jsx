1a57ed809e1db36d386563d2046a10e585fd0b72
Charles Bugayer <chaimbugayer2024@u.northwestern.edu>
2023-09-26 21:32:13 -0500

    fetch data

--- src/components/CourseCard.jsx
+++ src/components/CourseCard.jsx
@@ -3,8 +3,8 @@ import './CourseCard.css';
 const CourseCard = ({ course }) => (
     <div className="card m-1 p-2=">
         <div className="card-body">
-            <h5 className="card-title">{ course.title }</h5>
-            <p className="card-text">{ course.description }</p>
+            <h5 className="card-title">{ course.term } CS { course.number}</h5>
+            <h6 className="card-text">{ course.title }</h6>
         </div>
         <div className="card-footer">{ course.meets }</div>
     </div>
