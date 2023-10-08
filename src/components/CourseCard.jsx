3eb760f6e1dc46b3705aced35e2b8fe913808b11
Charles Bugayer <chaimbugayer2024@u.northwestern.edu>
2023-10-08 18:24:50 -0500

    add a course form in prog5

--- src/components/CourseCard.jsx
+++ src/components/CourseCard.jsx
@@ -18,21 +18,25 @@ const CourseCard = ({id,
     return (
         // if the course is selected, clicking will unselect it and call toggleUnselectables
         // if the course is unselectable, clicking will do nothing
-        <div className="product card m-1 p-2" onClick={() => 
-            {unselectables.includes(id)
-            ? null
-            : (toggleSelected(id), toggleUnselectables(id))}}>
-            <div className= {`card-body 
-                                ${selected.includes(id) ? 'selected' : ''} 
-                                ${unselectables.includes(id) ? 'unselectable' : ''}`}>
-                <h5 className="card-title">{ course.term } CS { course.number}</h5>
-                <h6 className="card-text">{ course.title }</h6>
+        <div>
+            <div className="product card m-1 p-2" onClick={() => 
+                {unselectables.includes(id)
+                ? null
+                : (toggleSelected(id), toggleUnselectables(id))}}>
+                <div className= {`card-body 
+                                    ${selected.includes(id) ? 'selected' : ''} 
+                                    ${unselectables.includes(id) ? 'unselectable' : ''}`}>
+                    <h5 className="card-title">{ course.term } CS { course.number}</h5>
+                    <h6 className="card-text">{ course.title }</h6>
+                </div>
+                <div className="card-footer">{ course.meets }</div>
+                <button className="btn btn-outline-secondary" onClick={openEditModal}>Edit</button>
             </div>
-            <div className="card-footer">{ course.meets }</div>
-            <button className="btn btn-outline-secondary" onClick={openEditModal}>Edit</button>
-            <Modal open={openEdit} close={closeEditModal}> 
-                <Edit id={id} course={course} />
-            </Modal>
+            <div className="product-list">
+                <Modal open={openEdit} close={closeEditModal}> 
+                    <Edit id={id} course={course} />
+                </Modal>
+            </div>
         </div>
     );
 };
